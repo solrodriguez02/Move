2ea6d37cd0ee3b218703991e4149ba34404ac42a
Sol Rodriguez <solrodriguez@MacBook-Pro-de-Sol.local>
2023-09-30 17:10:44 -0300

    aliases for icons

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -5,11 +5,12 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, md } from 'vuetify/iconsets/md'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
@@ -25,4 +26,25 @@ export default createVuetify({
       },
     },
   },
+
+  icons: {
+    defaultSet:'md',
+    aliases: {
+      ...aliases,
+      check: 'check_box',
+      warm: 'brightness_5', // sol del warm up
+      fire: 'local_fire_department', // fuego del ejercitamiento
+      cool: 'ac_unit', // copo de nieve del enfriamiento
+      rest: 'bedtime', // luna para el descanso
+      time: 'alarm', // reloj para el tiempo
+      space: 'zoom_out_map', // espacio para el ejercicio
+      person: 'accessibility',
+      flash: 'flash_on', // flash para la dificultad (con relleno)
+      dumbbell: 'fitness_center', // pesa
+    },
+    sets: {
+      md,
+    },
+  },
+
 })
