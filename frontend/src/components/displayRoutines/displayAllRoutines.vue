f21ef992edc37b766914c71e1b692ceab5c50109
agussang <msanguinetti@itba.edu.ar>
2023-10-04 18:46:02 -0300

    my routines almost ready

---
+++ frontend/src/components/displayRoutines/displayAllRoutines.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-expansion-panels >
+    <v-expansion-panel elevation="0" class="box mx-auto"   :value="1">
+      <v-expansion-panel-title/>
+      <v-expansion-panel-text  selected-class="bg-success" > 
+      <v-container class="pa-4">
+        <v-row v-for="i in 2" :key="i" >
+          <v-col
+          v-for="n in items"   :key="n"
+          cols="6"  md="3">
+          <v-card 
+            :class="['ma-4', selectedClass, 'cardRoutine']"
+            @click="toggle" 
+            color="grey-lighten-1"> 
+            <v-img 
+                  :src="`${n.src}`" 
+                  class="text-right pa-2 imgRoutine"
+                  @click="toggle">
+                  <v-btn icon="$edit" @click="changeFav()"/>            </v-img>
+            <p class='descripRoutine' style="text-overflow: ellipsis;">Holass sksksksksk sksksksk sksksksk sksk</p>
+          </v-card>
+          </v-col>
+        </v-row>
+        
+      </v-container>
+      </v-expansion-panel-text> 
+  </v-expansion-panel>
+</v-expansion-panels>
+ 
+  
+</template>
+
+<script scoped>
+export default {
+    data: () => ({
+      model: null,
+      items: [
+        {
+          src: 'backgrounds/bg.jpg',
+        },
+        {
+          src: 'backgrounds/md.jpg',
+        },
+        {
+          src: 'backgrounds/bg-2.jpg',
+        },
+        {
+          src: 'backgrounds/md2.jpg',
+        },
+      ],
+      selection: [],
+    })
+  }
+
+function changeFav(){
+  return
+}
+
+</script>
+
+<style scoped src='@/styles/previewRoutine/display.css'/>
---
+++ frontend/src/components/displayRoutines/displaySomeRoutines.vue
@@ -0,0 +1,62 @@
+<template>
+  
+  <v-sheet class="box mx-auto" >
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      selected-class="bg-success"
+      show-arrow
+    >
+      <v-slide-group-item
+        v-for="n in 10"
+        :key="n"
+        v-slot="{ isSelected, toggle, selectedClass }"
+      >
+        <v-card 
+          :class="['ma-4', selectedClass, 'cardRoutine']"
+          @click="toggle" 
+          color="grey-lighten-1"
+        >
+          <v-img 
+                :src="`${n.src}`" 
+                class="text-right pa-2 imgRoutine"
+                @click="toggle">
+                <v-btn :icon="isSelected ? '$edit' : '$time'" @click="changeFav()" />
+          </v-img>
+          <p class='descripRoutine' style="text-overflow: ellipsis;">Holass sksksksksk sksksksk sksksksk sksk</p>
+        </v-card>
+      </v-slide-group-item>
+    </v-slide-group>
+  </v-sheet>
+ 
+ 
+  
+</template>
+
+<script scoped>
+export default {
+  data: () => ({
+      model: null,
+      items: [
+        {
+          src: 'backgrounds/bg.jpg',
+        },
+        {
+          src: 'backgrounds/md.jpg',
+        },
+        {
+          src: 'backgrounds/bg-2.jpg',
+        },
+        {
+          src: 'backgrounds/md2.jpg',
+        },
+      ],
+      selection: [],
+    }) }
+function changeFav(){
+  return
+}
+
+</script>
+
+<style scoped src='@/styles/previewRoutine/display.css'/>
