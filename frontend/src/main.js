873a0c0ff88ab8d9a72dbffb706873cd306a9397
Sol Rodriguez <solrodriguez@MacBook-Pro-de-Sol.local>
2023-10-05 19:59:31 -0300

    Pinia in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,8 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
 import App from './App.vue'
 
-// Composables
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-// Plugins
 import { registerPlugins } from '@/plugins'
 import router from './router'
 
@@ -19,4 +11,6 @@ const app = createApp(App)
 registerPlugins(app)
 
 app.use(router)
+
+app.use(createPinia())
 app.mount('#app')
