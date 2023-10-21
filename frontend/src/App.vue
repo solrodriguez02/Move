16f43bcd247b6ba070027c323e4dbc6ca369a1de
UrielArias <urielarias.a12@gmail.com>
2023-10-20 21:01:29 -0300

    updating token

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,11 @@
 <script setup>
   import { RouterView } from 'vue-router'
   import Navbar from '@/components/Navbar.vue'
+  import { useSecurityStore } from './store/SecurityStore';
+  import { onBeforeMount } from 'vue';
+
+  const securityStore = useSecurityStore()
+  onBeforeMount(() => securityStore.initialize())
 </script>
 
 <style scoped>
