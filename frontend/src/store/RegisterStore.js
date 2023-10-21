e2b227998f8252b0fdf8692df949399cb39b5fca
solrodriguez02 <solrodriguez@itba.edu.ar>
2023-10-21 19:30:38 -0300

    user profile works

--- frontend/src/store/RegisterStore.js
+++ frontend/src/store/RegisterStore.js
@@ -1,19 +1,37 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { UserApi } from '@/api/user'
 
 export const useRegisterStore = defineStore('register', () => {
 
     const userInfo = ref({
-        id: '', 
-        email: '',
-        avatarUrl: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+        id: null, 
+        username: '',
+        email: '', 
+        firstName: '',
+        lastName: '', 
+        gender: null, 
+        avatarUrl: null,
     })
 
-    function setUserInfo(email, id) {
-        userInfo.value.id = id
+    function setUserEmail(email) {
         userInfo.value.email = email
     }
 
+    async function fetchCurrentUser() {
+        const currentUser = await UserApi.getCurrent(true)
+        setUserInfo(currentUser)
+    }
 
-    return { userInfo, setUserInfo }
+    function setUserInfo(currentUser) {
+        userInfo.value.id = currentUser.id
+        userInfo.value.username = currentUser.username
+        userInfo.value.email = currentUser.email
+        userInfo.value.firstName = currentUser.firstName
+        userInfo.value.lastName = currentUser.lastName
+        userInfo.value.gender = currentUser.gender
+        userInfo.value.avatarUrl = currentUser.avatarUrl
+    }
+
+    return { userInfo, setUserEmail, fetchCurrentUser }
 })
